92410c1b410dccdda233f62a75c23d631bebb987
qu1j0t3 <toby@telegraphics.com.au>
2014-04-27 23:20:28 -0400

    Successful build with sbt 0.7.7.

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,7 @@
 import sbt._
 import reaktor.scct.ScctProject
 
-class Project(info: ProjectInfo) extends DefaultProject(info) with ScctProject with posterous.Publish with ProguardProject {
+class Project(info: ProjectInfo) extends DefaultProject(info) with ScctProject {
   
   /** Paths */
   override def outputDirectoryName = "target"
@@ -11,13 +11,14 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ScctProject w
   val mavenLocal = "Local Maven Repository" at "file://D:/mvn-repository"
   val snapshotsRepo = "snapshots-repo" at "http://scala-tools.org/repo-snapshots"
 
-  val scalacheck    = "org.scala-tools.testing" % "scalacheck_2.9.0" % "1.9" 
-  val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" 
-  val scalazcore    = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2"
-  val hamcrest      = "org.hamcrest" % "hamcrest-all" % "1.1"
-  val mockito 	    = "org.mockito" % "mockito-all" % "1.8.5" 
-  val junit         = "junit" % "junit" % "4.7"
-  val pegdown       = "org.pegdown" % "pegdown" % "1.0.1"
+  val scalacheck    = "org.scalacheck" % "scalacheck_2.9.2" % "1.9" % "optional"
+  val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" % "optional"
+  val scalazcore    = "org.specs2" % "specs2-scalaz-core_2.9.1" % "6.0.1"
+  val hamcrest      = "org.hamcrest" % "hamcrest-all" % "1.1" % "optional"
+  val mockito 	    = "org.mockito" % "mockito-all" % "1.9.0" % "optional"
+  val junit         = "junit" % "junit" % "4.7" % "optional"
+  val pegdown       = "org.pegdown" % "pegdown" % "1.0.2" % "optional"
+  val specs2        = "org.specs2" % "classycle" % "1.4.1" % "optional"
   
   /** Compiling */
   override def compileOptions = Unchecked :: super.compileOptions.toList
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -6,8 +6,8 @@ class Plugins(info : ProjectInfo) extends PluginDefinition(info) {
    val tristanRepo    = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
    val codaRepo       = "Coda Hale's Repository" at "http://repo.codahale.com/"
 
-   val proguard   = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.5"
-   val assembly   = "com.codahale" % "assembly-sbt" % "0.1.1"
+   //val proguard   = "org.scala-tools.sbt" % "sbt-proguard-plugin" % "0.0.5"
+   //val assembly   = "com.codahale" % "assembly-sbt" % "0.1.1"
    val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
-   val posterous  = "net.databinder" % "posterous-sbt" % "0.1.7"
+   //val posterous  = "net.databinder" % "posterous-sbt" % "0.1.7"
 }
